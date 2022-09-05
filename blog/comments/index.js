b2a735191edef48c2105a79aff2d6c55420045c0
404th <umarov.doniyor.2002@gmail.com>
2022-09-05 18:17:11 +0500

    Some changes

---
+++ blog/comments/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const { randomBytes } = require("crypto");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const commentsByPostID = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+	res.send(commentsByPostID[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", async (req, res) => {
+	const randomId = randomBytes(4).toString("hex");
+
+	const { content } = req.body;
+
+	comments = commentsByPostID[req.params.id] || [];
+
+	comments.push({ id: randomId, content });
+
+	commentsByPostID[req.params.id] = comments;
+
+	await axios
+		.post("/events", {
+			type: "CommentCreated",
+			data: {
+				id: randomId,
+				content,
+				postId: req.params.id,
+			},
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+
+	res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+	const { type } = req.body;
+
+	console.log("Event type: ", type);
+
+	res.send({});
+});
+
+app.listen(4001, () => {
+	console.log("'comments' is running || PORT:4001");
+});
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const posts = {};
 
@@ -10,7 +13,7 @@ app.get("/posts", (req, res) => {
 	res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
 	const id = randomBytes(4).toString("hex");
 	const { title } = req.body;
 
@@ -19,7 +22,24 @@ app.post("/posts", (req, res) => {
 		title,
 	};
 
+	await axios
+		.post("http://localhost:4005/events", {
+			type: "PostCreated",
+			data: {
+				id,
+				title,
+			},
+		})
+		.catch((err) => console.log(err));
+
 	res.status(201).json(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+	const { type } = req.body;
+
+	console.log("Event type: ", type);
+	res.send({});
 });
 
 app.listen(4000, () => {
