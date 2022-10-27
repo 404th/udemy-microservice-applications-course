6c76cccf0c10dab3137636edbd90db09ba2a9ab2
404th <umarov.doniyor.2002@gmail.com>
2022-10-27 13:27:08 +0500

    some changes on skaffold

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,7 +3,11 @@ import express, { json } from "express"
 const app = express()
 app.use(json())
 
+app.get("/api/users/currentuser", (req, res) => {
+    res.send("Hi theredagilar!")
+})
+
 const AUTH_SERVICE_PORT = 3000
 app.listen( AUTH_SERVICE_PORT, () => {
-    console.log(`'auth' is listening on PORT:${ AUTH_SERVICE_PORT }`)
+    console.log(`'auth service' is listening on PORT:${ AUTH_SERVICE_PORT }`)
 } )
