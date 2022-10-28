f3e94135c8e2e58ea37cc3d0d7b9cadfd806d2a5
404th <umarov.doniyor.2002@gmail.com>
2022-10-28 21:51:43 +0500

    Some changes

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,11 +1,21 @@
 import express, { json } from "express"
 
+import { currentUserRouter } from "./routes/current-user"
+import { signinRouter } from "./routes/signin"
+import { signoutRouter } from "./routes/signout"
+import { signupRouter } from "./routes/signup"
+
+import { errorHandlerMid } from "./middlewares/error-handler"
+
 const app = express()
 app.use(json())
 
-app.get("/api/users/currentuser", (req, res) => {
-    res.send("Hi there!")
-})
+app.use( currentUserRouter )
+app.use( signinRouter )
+app.use( signoutRouter )
+app.use( signupRouter )
+
+app.use( errorHandlerMid )
 
 const AUTH_SERVICE_PORT = 3000
 app.listen( AUTH_SERVICE_PORT, () => {
